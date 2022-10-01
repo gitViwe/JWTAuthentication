f5a1aae1a64e544338d5fd97a83f2a5c363ff5f6
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-10-01 10:28:11 +0200

    Add validator for RefreshTokenCommand

---
+++ BaseServer/Application/Feature/Identity/RefreshToken/RefreshTokenCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Feature.Identity.RefreshToken;
+
+public class RefreshTokenCommandValidator : AbstractValidator<RefreshTokenCommand>
+{
+    public RefreshTokenCommandValidator()
+    {
+        RuleFor(x => x.Token).NotEmpty();
+        RuleFor(x => x.RefreshToken).NotEmpty();
+    }
+}
