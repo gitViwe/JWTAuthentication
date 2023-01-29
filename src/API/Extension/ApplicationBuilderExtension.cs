1ea71c034a6bc41b2801405aacd74cf824bded00
Viwe Nkepu <viwe.nkepu@hotmail.com>
2023-01-29 11:40:15 +0200

    refactor

--- src/API/Extension/ApplicationBuilderExtension.cs
+++ src/API/Extension/ApplicationBuilderExtension.cs
@@ -18,33 +18,28 @@ internal static class ApplicationBuilderExtension
             {
                 var handlerFeature = context.Features.Get<IExceptionHandlerPathFeature>();
 
-                int statusCode = StatusCodes.Status500InternalServerError;
-                string response = JsonSerializer.Serialize(ProblemDetailFactory.CreateProblemDetails(context, statusCode));
+                string response = JsonSerializer.Serialize(ProblemDetailFactory.CreateProblemDetails(context, StatusCodes.Status500InternalServerError));
 
                 if (handlerFeature is not null)
                 {
                     if (handlerFeature.Error is ForbiddenException forbidden)
                     {
-                        statusCode = StatusCodes.Status403Forbidden;
-                        response = JsonSerializer.Serialize(ProblemDetailFactory.CreateProblemDetails(context, statusCode, forbidden.Detail));
+                        response = JsonSerializer.Serialize(ProblemDetailFactory.CreateProblemDetails(context, StatusCodes.Status403Forbidden, forbidden.Detail));
                         logger.LogWarning(forbidden, "A forbidden exception occurred. Problem detail: {response}", response);
                     }
                     else if (handlerFeature.Error is NotFoundException notFound)
                     {
-                        statusCode = StatusCodes.Status404NotFound;
-                        response = JsonSerializer.Serialize(ProblemDetailFactory.CreateProblemDetails(context, statusCode, notFound.Detail));
+                        response = JsonSerializer.Serialize(ProblemDetailFactory.CreateProblemDetails(context, StatusCodes.Status404NotFound, notFound.Detail));
                         logger.LogWarning(notFound, "A not found exception occurred. Problem detail: {response}", response);
                     }
                     else if (handlerFeature.Error is UnauthorizedException unauthorized)
                     {
-                        statusCode = StatusCodes.Status401Unauthorized;
-                        response = JsonSerializer.Serialize(ProblemDetailFactory.CreateProblemDetails(context, statusCode, unauthorized.Detail));
+                        response = JsonSerializer.Serialize(ProblemDetailFactory.CreateProblemDetails(context, StatusCodes.Status401Unauthorized, unauthorized.Detail));
                         logger.LogInformation(unauthorized, "An unauthorized exception occurred. Problem detail: {response}", response);
                     }
                     else if (handlerFeature.Error is ValidationException validation)
                     {
-                        statusCode = StatusCodes.Status400BadRequest;
-                        response = JsonSerializer.Serialize(ProblemDetailFactory.CreateValidationProblemDetails(context, statusCode, validation.ToDictionary()));
+                        response = JsonSerializer.Serialize(ProblemDetailFactory.CreateValidationProblemDetails(context, StatusCodes.Status400BadRequest, validation.ToDictionary()));
                         logger.LogInformation(validation, "A validation exception occurred. Problem detail: {response}", response);
                     }
                     else
