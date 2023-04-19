cb1817210178a68f27d0e606c0b91fe32143586f
Viwe Nkepu <viwe.nkepu@hotmail.com>
2023-04-19 07:32:51 +0200

    Add implementation to LocalImageHostingClient

--- src/Application/Common/DevelopmentMock/LocalImageHostingClient.cs
+++ src/Application/Common/DevelopmentMock/LocalImageHostingClient.cs
@@ -68,4 +68,60 @@ public class LocalImageHostingClient : IImageHostingClient
 
         return response;
     }
+
+    public async Task<ImgBBUploadResponse> UploadImageAsync(HttpContent httpContent, string fileName, int expirationInSeconds = 300)
+    {
+        var uniqueFileName = Generator.RandomString(length: 6) + Path.GetExtension(fileName);
+
+        var filePath = Path.Combine(_uploadFolderPath, uniqueFileName);
+
+        if (!Directory.Exists(_uploadFolderPath))
+        {
+            Directory.CreateDirectory(_uploadFolderPath);
+        }
+
+        // Create a new FileStream to write the uploaded file to the local server folder
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await httpContent.CopyToAsync(stream);
+        }
+
+        var response = new ImgBBUploadResponse
+        {
+            Success = true,
+            Status = 200,
+            Data = new()
+            {
+                Id = "Pr7TkcH",
+                Title = "19s-by-wlop-dc6nyxz",
+                UrlViewer = "https://ibb.co/Pr7TkcH",
+                Url = filePath,
+                DisplayUrl = filePath,
+                Width = 500,
+                Height = 500,
+                Size = 1057,
+                Time = 1681544384,
+                Expiration = 1681544384,
+                DeleteUrl = "https://ibb.co/Pr7TkcH/6f41969f415bd35a5730bb9da29ec623",
+                Image = new()
+                {
+                    Filename = fileName,
+                    Name = fileName.Split('.').FirstOrDefault()!,
+                    Mime = "image/png",
+                    Extension = fileName.Split('.').LastOrDefault()!,
+                    Url = filePath
+                },
+                Thumb = new()
+                {
+                    Filename = fileName,
+                    Name = fileName.Split('.').FirstOrDefault()!,
+                    Mime = "image/png",
+                    Extension = fileName.Split('.').LastOrDefault()!,
+                    Url = filePath
+                }
+            }
+        };
+
+        return response;
+    }
 }
